246171814eef253843414e36f5be52824473026e
karishma-eng <109726835+karishma-eng@users.noreply.github.com>
2022-08-03 08:31:35 -0700

    Update Bank.java

--- Bank.java
+++ Bank.java
@@ -1,4 +1,4 @@
-/*Assignment Question  6:Design a class for a bank database the database should support 
+/* Design a class for a bank database the database should support 
 the following operations.
 1. Deposit a certain amount into an account,
 2. Withdrawing a certain amount from an account,
